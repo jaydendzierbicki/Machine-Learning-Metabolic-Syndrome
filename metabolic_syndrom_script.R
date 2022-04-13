4a0816f870c61244ccf50c4a0018f4d24a28451f
jaydendzierbicki <85862916+jaydendzierbicki@users.noreply.github.com>
2022-04-14 08:57:04 +1000

    Commenccing Imputation.

--- metabolic_syndrom_script.R
+++ metabolic_syndrom_script.R
@@ -13,8 +13,11 @@ set.seed(234)
 # STEP 1:
 # Data prep
 # Check for nulls & decide on imputation
+# Ensure each varible is converted to apporiate data type
 # Create new data frames, one for supervised and one for unsupervised
-#
+# Implement first model - naive, LDA, QDA
+# Implement second model - PCA
+# Implement third model - Kmeans clustering
 
 
 # DATA PREP=====================================================================
@@ -31,7 +34,11 @@ sum(is.na(metabolic_df)) # We have 436 varibles missing
 sapply(metabolic_df, function(x) sum(is.na(x))) # Column wise summary of null values
 
 # Data Imputation ==============================================================
-# Based on above we have the following varibles containing NULL values
-# Maritial: If Null then we will replace with unkown
-# Income
-# Waist Circumfrance
+# https://towardsdatascience.com/7-ways-to-handle-missing-values-in-machine-learning-1a6326adf79e 
+# There are serveral methods we can employ to deal with various missing data 
+# Martial: We will imputate maritial if NULL -> unkown
+# Income: 
+# Waist Circumstance
+
+metabolic_df$Marital[is.na(metabolic_df$Marital)] <- "Unkown"
+  
