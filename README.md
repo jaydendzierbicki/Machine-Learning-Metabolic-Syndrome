389cfec21a41883714882b86bc33b3dbb4b2381f
jaydendzierbicki <85862916+jaydendzierbicki@users.noreply.github.com>
2022-04-14 12:12:01 +1000

    NAIVE BAYES - complete

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 This project will look at the application of machine learning in predicting patients with metabolic syndrome via supervised methods.
 
 In addition, this project will look at the application of clustering of metabolic syndrome via unsupervised methods.
+
+
+## NAIVE BAYES
+Naive Bayes was implimneted on all the data and assumption testing was conducted.
+
+....
+....
+....
--- metabolic_syndrom_script.R
+++ metabolic_syndrom_script.R
@@ -2,6 +2,7 @@
 library(dplyr)
 library(VIM) # Used for imputation
 
+
 # Set seed for reproducability - arbitrarily selected
 set.seed(234)
 #===============================================================================
@@ -23,7 +24,6 @@ set.seed(234)
 
 # DATA PREP=====================================================================
 
-# Load in data==================================================================
 metabolic_df <- read.csv("metabolic.csv")
 
 
@@ -51,21 +51,23 @@ metabolic_df$Marital[is.na(metabolic_df$Marital)] <- "Unkown"
 # !!!
 
 metabolic_df_ni <- metabolic_df # Retain DF without k-means
-metabolic_df <- kNN(metabolic_df, imp_var = T) # Default k = 5, retain imputation flag
-
-
-
-
-
+metabolic_df <- kNN(metabolic_df, imp_var = F) # Default k = 5, retain imputation flag
 
 # Overall data coercion =========================================================
 # Ensure data is of correct type
 str(metabolic_df) # We observe many factor varibles are of type character 
 
+metabolic_df <- metabolic_df %>% 
+  mutate(Sex = as.factor(Sex),
+         Marital = as.factor(Marital),
+         Race = as.factor(Race),
+         MetabolicSyndrome = as.factor(MetabolicSyndrome))
+str(metabolic_df) # All varibles are now printing correctly
 
+# Supervised data prep =========================================================
+metabolic_df_supervised <- metabolic_df
 
-
-
+# Unsupervised data prep =======================================================
 # For our unsupervised learning method we will only select continuous varibles, at this
 # stage we will retain seqn and metabolic syndrom status
 metabolic_df_unsupervised <- metabolic_df %>% 
@@ -83,3 +85,88 @@ metabolic_df_unsupervised <- metabolic_df %>%
 
 # Since we are dealing with units in different measurements we will apply
 # a scaling algorithm prior to completing PCA analysis 
+
+
+
+
+
+
+
+
+# SUPERVISED LEARNING ==========================================================
+
+
+
+
+# Assumption testing ===========================================================
+# Will look at the following and pick the one that has best assumptions
+# Naive Bayes
+# LDA
+# QDA
+# Logistic
+# K-means
+# Logit
+
+library(caret) # used to split data
+
+# NAIVE BAYES ==================================================================
+metabolic_df_supervised_nb <- metabolic_df_supervised %>% 
+  select(-seqn)
+split_nb <- createDataPartition(metabolic_df_supervised_nb$MetabolicSyndrome, p = 0.8, list = F)
+train_nb <- metabolic_df_supervised_nb[split, ]
+test_nb <- metabolic_df_supervised_nb[-split, ]
+c(nrow(train_nb), nrow(test_nb)) # 1922 TRAIN, 479 TEST
+
+# Check balance of data
+prop.table(table(train_nb$MetabolicSyndrome)) # Data is somewhat imbalanced, 35% NMS, 65% MBS
+
+# ASSUMPTION TESTING - Independence NB
+library(psych)
+pairs.panels(metabolic_df_supervised_nb)
+
+# !!! We observe some varibles are correlated to one another - violation !!!
+
+# DATA DISTRUBTUION - Gaussian Distribution 
+par(mfrow=c(3,2))
+hist(metabolic_df$Age) # Not normal
+hist(metabolic_df$Income) # Not normal
+hist(metabolic_df$Triglycerides) # Skewed left
+hist(metabolic_df$BloodGlucose) # Not normal
+
+# !!! We observe does not follow Gaussian Distrubtion, set kernel  = T
+
+# Implement NAIVE BAYES
+library(naivebayes)
+nb_g <- naive_bayes(MetabolicSyndrome ~ ., data = train_nb, usekernel = F)
+get_cond_dist(nb_g) # Observe distrubtion 
+nb_gf <- naive_bayes(MetabolicSyndrome ~ ., data = train_nb, usekernel = T)
+get_cond_dist(nb_gf) # Observe distrubtion used for non paramtertic version 
+plot(nb_g, which = "BloodGlucose") # Test discriminatory power
+
+# Produce confusion matrix for NAIVE BAYES
+confusionMatrix(predict(nb_g), train_nb$MetabolicSyndrome)
+confusionMatrix(predict(nb_gf), train_nb$MetabolicSyndrome)
+# Based on test data we observe that the non-parametric model is slightly better
+
+pred_g <- predict(nb_g, newdata = test_nb)
+confusionMatrix(pred_g, test_nb$MetabolicSyndrome)
+
+pred_gf <- predict(nb_gf, newdata = test_nb)
+confusionMatrix(pred_gf, test_nb$MetabolicSyndrome)
+
+# We observe that using kernal density plots in place of Gaussian improves our model
+# this was expected based on the assumption testing
+
+# NAIVE BAYES AURO
+library(pROC)
+roc_gaussian <- roc(as.numeric(test_nb$MetabolicSyndrome), as.numeric(pred_g)) # AUC 0.7264
+roc_kernal <- roc(as.numeric(test_nb$MetabolicSyndrome), as.numeric(pred_gf)) # AUC 0.7526
+
+################################################################################
+# NAIVE BAYES DISC
+# We observe that our assumptions have been violated, such as conditional independence 
+# amongst predictor variables, though Naive bayes can handle this generally 
+# We observe that our data is not Gausian Distrubtion - as such we can apply
+# a non-paramatric feture in the naivebayes() function - this resulted in a 
+# improved model, both in accuracy and AUC
+################################################################################
